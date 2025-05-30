81fb768406cbe80e48ad3435df972bf543243b7d
PecosWhiskey <161496523+PecosWhiskey@users.noreply.github.com>
2025-05-30 17:34:12 +0200

    ricreato package.json Co-authored-by: SaraG15 <SaraG15@users.noreply.github.com>

--- back_end/database/dbBiglietti.js
+++ back_end/database/dbBiglietti.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database("C:\\Users\\sarag\\Documents\\UNIPA\\PROGRAMMAZIONE WEB E MOBILE\\prog_prova_server\\database\\dbBiglietti.sqlite", (err) => {
+const db = new sqlite3.Database("database\\dbBiglietti.sqlite", (err) => {
     if (err) {
       console.error('Errore connessione al database BIGLIETTI:', err.message);
     } else {

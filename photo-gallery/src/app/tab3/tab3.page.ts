0ec5f65796f3c471b8398aa94ee2f539dc0166dc
SaraG15 <saragiacalone2004@gmail.com>
2025-06-20 14:07:37 +0200

    modiche tab3 html

--- photo-gallery/src/app/tab3/tab3.page.ts
+++ photo-gallery/src/app/tab3/tab3.page.ts
@@ -1,49 +1,52 @@
 import { Component } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonTabs,
-  IonTabBar,
-  IonTabButton,
-  IonIcon,
-  IonLabel,
-  IonButton,
-  IonCard,
-  IonCardContent,
-  IonSegment,
-  IonSegmentButton,
-  IonItem,
-  IonInput
-} from '@ionic/angular/standalone';
+import {IonHeader,IonToolbar,IonTitle,IonContent,IonTabs,IonTabBar,IonTabButton,IonIcon,IonLabel,IonButton,IonCard,
+  IonCardContent,IonSegment,IonSegmentButton,IonItem,IonInput, IonSelectOption, IonSelect} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    ExploreContainerComponent,
-    IonTabs,
-    IonTabBar,
-    IonTabButton,
-    IonIcon,
-    IonLabel,
-    IonButton,
-    IonCard,
-    IonCardContent,
-    IonSegment,
-    IonSegmentButton,
-    IonItem,
-    IonInput
-  ],
+  imports: [IonHeader,IonToolbar,IonTitle,IonContent,ExploreContainerComponent,IonTabs,IonTabBar,IonTabButton,IonIcon,IonLabel,
+    IonButton,IonCard,IonCardContent,IonSegment,IonSegmentButton,IonItem,IonInput, FormsModule, IonSelectOption, IonSelect],
 })
 export class Tab3Page {
   constructor() {}
+
+  idCliente = ''; //codice fiscale del cliente che prendiamo come id
+  nome = '';
+  cognome = '';
+  dataNascita = '';
+  documentoID = '';
+  sesso = '';
+  nazionalita = '';
+  stato = '';
+  citta= '';
+  CAP = '';
+  indirizzo = '';
+  numCivico = 0;
+  disabile = 0;
+  email = '';
+  password= '';
+
+  form= 'Login'; //di default assume valore 'login' se l'utente clicca sul bottone 'Registrati' cambai in 'registrazione
+//                 e viene mostrato il form per la registrazione
+
+  changeForm(){
+    this.form = 'Registrazione';
+  }
+
+  back(){
+    this.form = 'Login';
+  }
+
+  Registrazione(){
+    console.log('registrato');
+  }
+
+  Login(){
+    console.log('accesso effetuato');
+  }
 }
